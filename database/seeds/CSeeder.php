f5f3c852ff728905d8b4cb3019a8ca8233e1b369
hpn33 <hamed.hpn332@gmail.com>
2020-02-13 19:19:57 +0330

    seed

---
+++ database/seeds/CSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
