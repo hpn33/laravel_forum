c58c4eb834713ed539df9658c6f831f13ea8703f
hpn33 <hamed.hpn332@gmail.com>
2020-04-19 14:17:52 +0430

    extra fix

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,17 +8,6 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
-
-    private $oldExceptionHandler;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-//        $this->disableExceptionHandling();
-    }
-
-
     protected function signIn($user = null) {
 
         $user = $user ?: create('App\User');
@@ -26,33 +15,4 @@ abstract class TestCase extends BaseTestCase
         return $this->actingAs($user);
     }
 
-
-    protected function disableExceptionHandling()
-    {
-
-        $this->oldExceptionHandler = $this->app->make(ExceptionHandler::class);
-
-        $this->app->instance(ExceptionHandler::class, new class extends Handler {
-            public function __construct() {}
-
-            public function report(\Exception $exception) {}
-
-            public function render($request, \Exception $exception)
-            {
-                throw $exception;
-            }
-
-        });
-
-    }
-
-
-    public function withExceptionHandling()
-    {
-
-        $this->app->instance(ExceptionHandler::class, $this->oldExceptionHandler);
-
-        return $this;
-
-    }
 }
