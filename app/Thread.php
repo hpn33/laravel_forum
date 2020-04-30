275089fd7930bd3c3c458fa8368fc541c8e728b8
hpn33 <hamed.hpn332@gmail.com>
2020-04-30 07:15:06 +0430

    016-Meta Details and Pagination

--- app/Thread.php
+++ app/Thread.php
@@ -13,6 +13,19 @@ class Thread extends Model
     protected $guarded = [];
 
 
+    /**
+     * Boot the model.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('replyCount', function ($builder) {
+            $builder->withCount('replies');
+        });
+    }
+
+
     public function path($extension = '')
     {
 
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -1,50 +1,69 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">
-                    {{ $thread->title }}
-                    <br>
-                    by <a href="#">{{ $thread->creator->name }}</a>
-                </div>
-
-                <div class="card-body">
-                    {{ $thread->body }}
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            
-            @foreach ($thread->replies as $reply)
-
-                @include('threads.reply')
-            
-            @endforeach
-        </div>
-    </div>
-
-    @if(auth()->check())
-        <div class="row justify-content-center" style="margin-top: 50px">
+    <div class="container">
+        <div class="row">
             <div class="col-md-8">
-                <form method="POST" action="{{ $thread->path('replies') }}">
-                    @csrf
-
-                    <div class='form-group'>
-                        <textarea name='body' id='body' class="form-control" placeholder="Have something to say?" rows="5"></textarea>
+                <div class="card">
+                    <div class="card-header">
+                        {{ $thread->title }}
+                        <br>
+                        by <a href="#">{{ $thread->creator->name }}</a>
                     </div>
-                    
-                    <button type="submit" class="btn btn-default">Post</button>
-                </form>
 
+                    <div class="card-body">
+                        {{ $thread->body }}
+                    </div>
+                </div>
             </div>
         </div>
-    @else
-        <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
-    @endif
-</div>
+        <div class="row">
+            <div class="col-md-8">
+
+                @foreach ($replies as $reply)
+
+                    @include('threads.reply')
+
+                @endforeach
+
+                {{ $replies->links() }}
+
+                @if(auth()->check())
+                    <form method="POST" action="{{ $thread->path('replies') }}">
+                        @csrf
+
+                        <div class='form-group'>
+                            <textarea name='body' id='body' class="form-control" placeholder="Have something to say?"
+                                      rows="5"></textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-default">Post</button>
+                    </form>
+
+                @else
+                    <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this
+                        discussion.
+                    </p>
+                @endif
+            </div>
+
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+
+                        <p>
+                            This Thread was published {{ $thread->created_at->diffForHumans() }} by
+                            <a href="">{{ $thread->creator->name }}</a>, and currently
+                            has {{ $thread->replies_count }} {{ \Illuminate\Support\Str::plural('comment', $thread->replies_count) }}
+                            .
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+    </div>
 @endsection
